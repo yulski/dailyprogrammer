d3eb851e616d3a0613943f88acb8b8a51993217f
yulski <julexmma@gmail.com>
2017-09-09 22:43:03 +0100

    added solution for 330

---
+++ sorround-the-circles/README.md
@@ -0,0 +1,11 @@
+# [[2017-09-04] Challenge #330 [Easy] Surround the circles](https://www.reddit.com/r/dailyprogrammer/comments/6y19v2/20170904_challenge_330_easy_surround_the_circles/)
+
+## Description 
+In this challenge, you will be given a set of circles, defined by their centers and radii. Your goal is to find the bounding rectangle which will contain all of the circles completely.
+Write a program that determines the vertices of the bounding rectangle with sides parallel to the axes.
+
+## Challenge Input 
+1,1,2  
+2,2,0.5  
+-1,-3,2  
+5,2,1
---
+++ sorround-the-circles/sorround-the-circles.js
@@ -0,0 +1,49 @@
+const input = [
+	[1, 1, 2],
+	[2, 2, 0.5],
+	[-1, -3, 2],
+	[5, 2, 1]
+];
+
+let lowestX = 0,
+	highestX = 0,
+	lowestY = 0,
+	highestY = 0;
+
+for(let arr of input) {
+	let x = arr[0],
+		y = arr[1],
+		r = arr[2];
+	if((x - r) < lowestX) {
+		console.log('changing lowestX');
+		lowestX = (x - r);
+	}
+	if((x + r) > highestX) {
+		console.log('changing highestX');
+		highestX = (x + r);
+	}
+	if((y - r) < lowestY) {
+		console.log('changing lowestY');
+		lowestY = (y - r);
+	}
+	if((y + r) > highestY) {
+		console.log('changing highestY');
+		highestY = (y + r);
+	}
+}
+/*
+a------------b
+|            |
+|            |
+|            |
+c------------d
+*/
+let ans = {
+	a: {x: lowestX, y: highestY},
+	b: {x: highestX, y: highestY},
+	c: {x: lowestX, y: lowestY},
+	d: {x: highestX, y: lowestY},
+	
+};
+
+console.log(ans);
