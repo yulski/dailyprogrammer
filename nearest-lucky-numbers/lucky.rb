f4e92bb46358206c4ca9b1ad9d6a556a817443d4
yulski <julexmma@gmail.com>
2017-09-18 00:06:00 +0100

    fixed lucky numbers solution based on a nice solution I found on Reddit

--- nearest-lucky-numbers/lucky.rb
+++ nearest-lucky-numbers/lucky.rb
@@ -1,68 +1,32 @@
-=begin
-    TODO: Optimize this - this solution works but is horrible. VERY slow.
-    Input 100,000 takes almost 2 minutes, where it should take a few seconds.
-    There is a C solution where 100,000 takes 1.7 secs, another C solution that
-    takes 0.3 secs, a Python one that takes around 8 secs and another Python one
-    that takes 0.02 seconds for 100,000.
-=end
+# this solution was "inspired" by a nice and easy C# solution in the comments of the challenge 
+ARGV.each do |num|
+    num = num.to_i
+    range = num * 2
+    step = 1
+    arr = Array 1..range
 
-def lucky_nums_to(n)
-    arr = Array 1..n
-    index = 0
-    inc = 2
-    lim = arr.size
+    j = 1
     loop do
-        i = inc - 1
-        to_d = []
-        while i < lim
-            to_d.push i
-            i += inc
-        end
-        c = []
-        j = 0
-        while j < arr.size
-            c.push arr[j] unless to_d.include?(j)
-            j += 1
-        end
-        arr = c
-        index += 1
-        inc = arr[index]
-        lim = arr.size
-        break if inc >= lim
+        arr.delete_at j
+        break if j >= arr.size
+        j += 1
     end
-    arr
-end
 
-def lucky_nums_from(n)
-    to_nums = lucky_nums_to n
-    before = to_nums.last
-    after = nil
-    x = 2
-    nums = []
-    loop do
-        nums = lucky_nums_to(n * x)
-        x += 1
-        if nums.size > to_nums.size
-            after = nums.first
-            break
+    while step < arr.size
+        i = arr[step] - 1
+        loop do
+            arr.delete_at i
+            break if i >= arr.size
+            i += arr[step] - 1
         end
+        step += 1
     end
-    from_nums = []
-    nums.each do |i|
-        from_nums.push i if i > n
-    end
-    from_nums
-end
 
-input = [103, 225, 997]
-input.each do |i|
-    to = lucky_nums_to i
-    from = lucky_nums_from i
-    if to.include? i
-        puts "#{i} is a lucky number"
-        exit
+    if arr.include?(num)
+        puts num.to_s + " is a lucky number"
+    else
+        after_index = arr.index { |x| x > num }
+        before_index = after_index - 1
+        puts arr[before_index].to_s + " < " + num.to_s + " < " + arr[after_index].to_s
     end
-    prev = to.last
-    nxt = from.first
-    puts "#{prev} < #{i} < #{nxt}"
 end
